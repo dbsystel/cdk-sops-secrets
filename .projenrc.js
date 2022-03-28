4ca3da253d22fc3f4da73a72a2af5f955b1b6ad0
Markus <mail@markussiebert.com>
2022-03-28 18:43:43 +0200

    fix

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,13 @@
-const { awscdk } = require("projen");
+const { awscdk } = require('projen');
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: "Markus Siebert",
-  authorAddress: "dev@markussiebert.com",
-  cdkVersion: "2.1.0",
-  defaultReleaseBranch: "main",
+  author: 'Markus Siebert',
+  authorAddress: 'dev@markussiebert.com',
+  cdkVersion: '2.1.0',
+  defaultReleaseBranch: 'main',
 
-  name: "cdk-sops-secrets",
-  repositoryUrl: "https://github.com/markussiebert/cdk-sops-secrets.git",
-  peerDeps: [
-
-  ]
+  name: 'cdk-sops-secrets',
+  repositoryUrl: 'https://github.com/markussiebert/cdk-sops-secrets.git',
+  peerDeps: [],
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
@@ -17,87 +15,87 @@ const project = new awscdk.AwsCdkConstructLibrary({
 });
 
 project.buildWorkflow.preBuildSteps.unshift({
-  name: "list artifacts",
-  run: "ls -la dist/*"
-})
+  name: 'list artifacts',
+  run: 'ls -la dist/*',
+});
 
 project.buildWorkflow.preBuildSteps.unshift({
-  name: "download go artifact",
-  uses: "actions/download-artifact@v2",
+  name: 'download go artifact',
+  uses: 'actions/download-artifact@v2',
   with: {
-    name: "build-artifact-go",
-    path: "dist/*",
+    name: 'build-artifact-go',
+    path: 'dist/*',
   },
-})
+});
 
 console.log(project.github.workflows.map((wr) => wr.name));
 
 const fixme = project.github.workflows.filter((wf) =>
-  ["build", "release"].includes(wf.name)
+  ['build', 'release'].includes(wf.name),
 );
 fixme.forEach((wf) => {
   Object.keys(wf.jobs).forEach((key) => {
-    if(key === "build") {
-      console.log(wf.jobs[key])
+    if (key === 'build') {
+      console.log(wf.jobs[key]);
     } else {
       wf.jobs[key].steps.unshift({
-        name: "download go artifact",
-        uses: "actions/download-artifact@v2",
+        name: 'download go artifact',
+        uses: 'actions/download-artifact@v2',
         with: {
-          name: "build-artifact-go",
-          path: "dist/*",
+          name: 'build-artifact-go',
+          path: 'dist/*',
         },
-      },)
+      });
     }
-    wf.jobs[key] = {...wf.jobs[key], needs: 'goreleaser'}
+    wf.jobs[key] = { ...wf.jobs[key], needs: 'goreleaser' };
   });
-  wf.addJob("goreleaser", {
-    name: "goreleaser",
-    runsOn: "ubuntu-latest",
+  wf.addJob('goreleaser', {
+    name: 'goreleaser',
+    runsOn: 'ubuntu-latest',
     on: {
       pull_request: null,
       push: null,
     },
     permissions: {
-      contents: "write",
+      contents: 'write',
     },
     steps: [
       {
-        name: "Checkout",
-        uses: "actions/checkout@v2",
+        name: 'Checkout',
+        uses: 'actions/checkout@v2',
         with: {
-          "fetch-depth": 0,
+          'fetch-depth': 0,
         },
       },
       {
-        name: "Fetch all tags",
-        run: "git fetch --force --tags",
+        name: 'Fetch all tags',
+        run: 'git fetch --force --tags',
       },
       {
-        name: "Set up Go",
-        uses: "actions/setup-go@v2",
+        name: 'Set up Go',
+        uses: 'actions/setup-go@v2',
         with: {
-          "go-version": 1.18,
+          'go-version': 1.18,
         },
       },
       {
-        name: "Run GoReleaser",
-        uses: "goreleaser/goreleaser-action@v2",
+        name: 'Run GoReleaser',
+        uses: 'goreleaser/goreleaser-action@v2',
         with: {
-          distribution: "goreleaser",
-          version: "latest",
-          args: "release --rm-dist",
+          distribution: 'goreleaser',
+          version: 'latest',
+          args: 'release --rm-dist',
         },
         env: {
-          GITHUB_TOKEN: "${{ secrets.GITHUB_TOKEN }}",
+          GITHUB_TOKEN: '${{ secrets.GITHUB_TOKEN }}',
         },
       },
       {
-        name: "Upload artifact",
-        uses: "actions/upload-artifact@v2.1.1",
+        name: 'Upload artifact',
+        uses: 'actions/upload-artifact@v2.1.1',
         with: {
-          name: "build-artifact-go",
-          path: "dist/*",
+          name: 'build-artifact-go',
+          path: 'dist/*',
         },
       },
     ],
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="cdk-sops-secrets.Hello"></a>
+
+#### Initializers <a name="Initializers" id="cdk-sops-secrets.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from 'cdk-sops-secrets'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#cdk-sops-secrets.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="cdk-sops-secrets.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
