7b4092445622eaf6ef78494b6629294fcb3f9917
Markus <mail@markussiebert.com>
2022-03-28 20:59:46 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,39 @@
-export class Hello {
-  public sayHello() {
-    return 'hello, world!';
+import { IGrantable, IPrincipal } from '@aws-cdk/aws-iam';
+import { Construct, CustomResource, CustomResourceProvider } from '@aws-cdk/core';
+import { Code, Function } from '@aws-cdk/aws-lambda';
+import * as cr from '@aws-cdk/custom-resources';
+import { Provider } from '@aws-cdk/custom-resources';
+
+export interface SopsSecretProps {
+
+}
+export class SopsSecrets extends Construct {
+
+  public constructor(scope: Construct, id: string, props: SopsSecretProps) {
+    super(scope, id);
+    new CustomResource(this, 'Resource', {
+
+    })
   }
+
+}
+export interface SopsSecretsProviderProps {
+
+}
+export class SopsSecretsProvider extends Construct implements IGrantable {
+  
+  private grantPrincipal: IPrincipal;
+
+  public constructor(scope: Construct, id: string, props: SopsSecretsProviderProps ) {
+    super(scope, id);
+    const lambda = new Function(this, 'Function', {
+      code: Code.fromAsset(../dist/)
+    })
+    const cr = new Provider(this, 'Resource', {
+      onEventHandler:
+    })
+
+    })
+  }
+  
 }
