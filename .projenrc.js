a8168a74b507f30f6c7b3517efe6daefe0952fc5
Markus <mail@markussiebert.com>
2022-03-31 18:16:39 +0200

    fix

--- .projenrc.js
+++ .projenrc.js
@@ -55,8 +55,8 @@ additionalActions = [
     run: 'mkdir -p assets && mkdir -p __w/cdk-sops-secrets/assets && zip assets/cdk-sops-lambda.zip ./dist-goreleaser/cdk-sops-secrets_linux_amd64/cdk-sops-lambda && cp assets/cdk-sops-lambda.zip __w/cdk-sops-secrets/assets/cdk-sops-lambda.zip ',
   },
   {
-    name: 'List artifacts',
-    run: 'ls -ld $(find ./assets)',
+    name: 'List Everything',
+    run: 'ls -ld $(find .)',
   },
 ];
 
