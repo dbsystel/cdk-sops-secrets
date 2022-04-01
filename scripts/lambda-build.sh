7cd09d1d42c4282f651094ff2b9e4a91d9a4e68e
Markus <mail@markussiebert.com>
2022-04-01 22:04:59 +0200

    test

--- scripts/lambda-build.sh
+++ scripts/lambda-build.sh
@@ -5,7 +5,7 @@ cd "$BASEPATH/lambda"
 GOOS=linux
 GOARCH=amd64
 GOPROXY=https://proxy.golang.org,direct
-go build -trimpath -ldflags="-s -w -buildid="
+go build -trimpath -buildvcs=false -ldflags="-s -w -buildid="
 ls -la cdk-sops-secrets
 shasum cdk-sops-secrets
 touch -t 202002020000 cdk-sops-secrets
