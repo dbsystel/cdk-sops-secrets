e80ec48a2197a123dc5cfe7170366e4edce172aa
Markus <mail@markussiebert.com>
2022-04-01 16:50:33 +0200

    test

--- scripts/build-lambda-in-docker.sh
+++ scripts/build-lambda-in-docker.sh
@@ -3,5 +3,7 @@
 BASEPATH=$(git rev-parse --show-toplevel)
 
 $BASEPATH/scripts/clean.sh
-docker run -v $BASEPATH:/__w/cdk-sops-secrets/cdk-sops-secrets golang:1.17.8-buster /bin/bash -c "cd /__w/cdk-sops-secrets/cdk-sops-secrets && scripts/clean.sh && scripts/lambda-build.sh"
+docker run -v $BASEPATH:/__w/cdk-sops-secrets/cdk-sops-secrets --platform linux/amd64 golang:1.18-buster /bin/bash -c "cd /__w/cdk-sops-secrets/cdk-sops-secrets && scripts/clean.sh && scripts/lambda-build.sh" -m
+x86_64
 $BASEPATH/scripts/lambda-zip.sh
+npx projen integ:secret:snapshot
