9301a0276e13c4d0d503dc7141ce222e5ff27b3f
Markus <mail@markussiebert.com>
2022-04-06 23:56:19 +0200

    fix

--- .projenrc.js
+++ .projenrc.js
@@ -10,7 +10,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
     'kms',
     'gitops',
     'secrets management',
-    'secrets'
+    'secrets',
   ],
   cdkDependenciesAsDeps: false,
   defaultReleaseBranch: 'main',
