6e147b7e5c127ca94df5ec8159f85495d0c6ac82
Markus <mail@markussiebert.com>
2022-04-11 22:06:23 +0200

    test

--- src/index.ts
+++ src/index.ts
@@ -184,7 +184,7 @@ export class SopsSync extends Construct {
 
     let fileHash:string = FileSystem.fingerprint(props.sopsFilePath);
     try {
-      const commit = execSync(`git log -n 1 --pretty=format:%H -- ${props.sopsFilePath}`).toString()
+      const commit = execSync(`git log --no-merges -n 1 --pretty=format:%H -- ${props.sopsFilePath}`).toString()
       // Check if the result is a valid git commit
       if (commit.match("^[a-fA-F0-9]{40}$")?.length == 1) {
         fileHash = commit
