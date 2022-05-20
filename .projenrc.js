3845726dbed1b72c07087b6109813bad25cb5001
Markus <mail@markussiebert.com>
2022-05-20 23:04:44 +0200

    projen allow renovate

--- .projenrc.js
+++ .projenrc.js
@@ -18,7 +18,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   npmignoreEnabled: true,
   autoApproveUpgrades: true,
   autoApproveOptions: {
-    allowedUsernames: ['markussiebert'],
+    allowedUsernames: ['markussiebert', 'renovate'],
   },
   name: 'cdk-sops-secrets',
   repositoryUrl: 'https://github.com/markussiebert/cdk-sops-secrets.git',
