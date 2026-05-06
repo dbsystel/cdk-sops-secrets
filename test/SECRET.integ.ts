e698d4a3f3a5363a1d57e20e3921ca5b7fb7ef9c
Dario Zachow <dario.zachow@deutschebahn.com>
2026-05-06 14:13:44 +0200

    WIP: test integration with expiration

--- test/SECRET.integ.ts
+++ test/SECRET.integ.ts
@@ -1,6 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { ConstantAssetHashAspect } from './ConstantAssetHashAspect';
-import { RawOutput, SopsSecret } from '../src/index';
+import { RawOutput, SopsSecret, SopsSecretProps } from '../src/index';
 
 const app = new cdk.App();
 const stack = new cdk.Stack(app, 'SECRET');
@@ -11,7 +11,7 @@ cdk.Aspects.of(stack).add(new ConstantAssetHashAspect());
 interface TestCase {
   name: string;
   sopsFilePath: string;
-  additionalProperties: Record<string, any>;
+  additionalProperties: SopsSecretProps;
 }
 
 const tc = [
@@ -30,7 +30,11 @@ const tc = [
   {
     name: 'Yaml2Json',
     sopsFilePath: 'test-secrets/testsecret.sops.yaml',
-    additionalProperties: {},
+    additionalProperties: {
+      expiration: {
+        daysBeforeExpiration: 9,
+      },
+    },
   },
   {
     name: 'Yaml2RawString',
